3f796d503b4bf14421c7ca4bf54895786f814b19
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2017-01-06 16:42:59 -0500

    in, out

--- parse_rep.rb
+++ parse_rep.rb
@@ -2,4 +2,4 @@ require 'json'
 require 'ostruct'
 
 tiger = JSON.parse(File.read('./examples/zoo/representations/tiger.json'), object_class: OpenStruct)
-p tiger.affordances.map(&:name)
+p tiger.affordances.map(&:name).zip(tiger.affordances.map(&:return_type))
