24e3591063043a2161474da7a85f8a68d83dbd8f
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2017-01-06 16:49:04 -0500

    affordances by type

--- parse_rep.rb
+++ parse_rep.rb
@@ -2,4 +2,5 @@ require 'json'
 require 'ostruct'
 
 tiger = JSON.parse(File.read('./examples/zoo/representations/tiger.json'), object_class: OpenStruct)
-p tiger.affordances.map(&:name).zip(tiger.affordances.map(&:return_type))
+hash = { "#{tiger.representation_type}": Hash[tiger.affordances.map(&:name).zip(tiger.affordances.map(&:return_type))] }
+p hash
