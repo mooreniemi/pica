cb793322f206f2540b91724e4a7b6cd96471adbb
Alex Moore-Niemi <alex.moore-niemi@raizlabs.com>
2017-01-09 17:53:01 -0500

    maybe

--- examples/zoo/zoo.rb
+++ examples/zoo/zoo.rb
@@ -1,20 +1,20 @@
 require 'sinatra/base'
+require './resources/animal_type.rb'
+require './resources/tigers.rb'
 require './resources/animals.rb'
 require './resources/add_animal.rb'
-require './resources/animal_type.rb'
 require './resources/tiger.rb'
 require './resources/free_him.rb'
 require './resources/feed_him.rb'
-require './resources/tigers.rb'
 
 
 class ZooApp < Sinatra::Application
+  register Sinatra::Zoo::Resources::AnimalType
+  register Sinatra::Zoo::Resources::Tiger
   register Sinatra::Zoo::Resources::Animal
   register Sinatra::Zoo::Resources::AddAnimal
-  register Sinatra::Zoo::Resources::AnimalType
   register Sinatra::Zoo::Resources::Tiger
   register Sinatra::Zoo::Resources::FreeHim
   register Sinatra::Zoo::Resources::FeedHim
-  register Sinatra::Zoo::Resources::Tiger
 
 end
